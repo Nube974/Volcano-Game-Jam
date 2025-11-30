76be170846ffd030a1c87865c8dbf80ac4e8875a
Sylvie HOAREAU <hoareausms@gmail.com>
2025-11-30 08:08:12 +0400

    fix: correction du SceneLoader

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,6 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    
     public void LoadLevel()
     {
         SceneManager.LoadScene(1);
